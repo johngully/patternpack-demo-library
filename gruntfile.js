2a10e403b8202977e8fb3ea17a9bc5fc45400f9b
John Gully <john@gully.us>
2016-02-26 22:28:16 -0600

    Step 8
    Establish new pattern hierarchy and move the data-form pattern

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function (grunt) {
   grunt.initConfig({
     patternpack: {
+      options: {
+        patternStructure: [
+          { "name": "Atoms", "path": "atoms" },
+          { "name": "Molecules", "path": "molecules" },
+          { "name": "Organisms", "path": "organisms" },
+          { "name": "Templates", "path": "templates" },
+          { "name": "Pages", "path": "pages" }
+        ]
+      },
       run: {},
       build: {},
       release: {}
